6bcf5c10cc6cc43fcfc94b4320397dbfb0592e73
sandeshAdhikary <adhikary.sandesh@gmail.com>
2023-10-19 14:12:22 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='trainer', 
       version='1.0', 
-      packages=find_packages(),
-      package_data={'trainer': ['trainer/.config.yaml']}
+      packages=find_packages()
       )
