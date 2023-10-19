79b05b34b77c983846edd492933aeb0a0c6c5c4e
sandeshAdhikary <adhikary.sandesh@gmail.com>
2023-10-19 14:40:42 -0700

    back to old structure

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(name='trainer',
       version='1.0', 
       packages=find_packages(where="src"),
       package_dir={"": "src"},
-      include_package_data=True
+      include_package_data=True,
+      package_data={
+          "": ["*.yaml"],
+      }
       )
