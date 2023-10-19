35703f21139f9cf3af191b8c8fcd44f447001209
sandeshAdhikary <adhikary.sandesh@gmail.com>
2023-10-19 14:28:03 -0700

    back to old structure

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name='trainer', 
       version='1.0', 
       packages=find_packages(),
-    #   package_dir={"", "src"},
+      package_dir={"", "trainer"},
       include_package_data=True
       )
