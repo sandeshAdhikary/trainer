d029a5d4084573fd203ae3d177b7f97367534f51
sandeshAdhikary <adhikary.sandesh@gmail.com>
2024-08-11 20:33:46 -0700

    deleted repeated files

--- default_trainers.py
+++
@@ -1,9 +0,0 @@
-from trainer.rl import RLTrainer
-
-class DefaultRLTrainer(RLTrainer):
-
-    def train_step(self, batch, batch_idx):
-        pass
-
-    def log_epoch(self, info):
-        pass
--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='trainer', 
-      version='2.0', 
-      packages=find_packages(),
-      )
